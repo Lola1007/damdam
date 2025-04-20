7ef21718f52641f06c241306760c6ff5ef669c01
Lola1007 <164361211+Lola1007@users.noreply.github.com>
2025-04-20 11:57:46 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+// script.js
+document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Message sent! We'll get back to you soon.");
+    this.reset();
+  });
+  
